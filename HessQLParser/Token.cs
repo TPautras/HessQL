13d67c156155a28e0b6fb9eed63d169c0a649973
TPautras <155535273+TPautras@users.noreply.github.com>
2025-04-09 00:17:04 +0200

    tests...

--- HessQLParser/Token.cs
+++ HessQLParser/Token.cs
@@ -1,7 +1,22 @@
 ﻿namespace HessQLParser;
 
 public class Token
-{
-    public enum TokenType { Get; set; }
+{ 
+    public TokenTypes TokenType;
+    public string Value;
+
+    public Token(string value, TokenTypes type)
+    {
+        this.Value = value;
+        this.TokenType = type;
+    }
+
+    public enum TokenTypes
+    {
+        Test, 
+        test, 
+        ak
+    }
+    
     
 }
