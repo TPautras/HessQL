e0381e2ea01f89d4157212242baffd4744c4fd4e
TPautras <155535273+TPautras@users.noreply.github.com>
2025-04-09 14:54:09 +0200

    add: Tokens and UnitTests

---
+++ HessQLParser/Tokens/Token.cs
@@ -0,0 +1,34 @@
+﻿namespace HessQLParser;
+
+public partial class Token(string value, Token.TokenTypes type)
+{ 
+    public TokenTypes TokenType = type;
+    public string Value = value;
+
+    public static Token NewToken(string value, Token.TokenTypes type)
+    {
+        return new Token(value,type);
+    }
+
+    public bool IsOneOfMany(List<TokenTypes> tokensTypes)
+    {
+        return tokensTypes.Contains(this.TokenType);
+    }
+
+    public static string Debug(Token token)
+    {
+        List<TokenTypes> tokensTypes = new List<TokenTypes>();
+        tokensTypes.Add(TokenTypes.IDENTIFIER);
+        tokensTypes.Add(TokenTypes.NUMERIC);
+        tokensTypes.Add(TokenTypes.STRING_LITERAL);
+        string res = "";
+        if (token.IsOneOfMany(tokensTypes))
+            res = (TokenKindToString(token.TokenType) + ": " + token.Value);
+        else
+        {
+           res = TokenKindToString(token.TokenType);
+        }
+
+        return res;
+    }
+}
---
+++ UnitTesting/Parser/TokensTest.cs
@@ -0,0 +1,22 @@
+﻿using HessQLParser;
+
+namespace UnitTesting.Parser;
+
+public class TokensTests
+{
+    [Test]
+    public void Test_Init()
+    {
+        Token myTokenEof = Token.NewToken("", Token.TokenTypes.END_OF_FILE);
+        Assert.That(myTokenEof.TokenType, Is.EqualTo(Token.TokenTypes.END_OF_FILE));
+    }
+
+    [Test]
+    public void Test_Debug()
+    {
+        Token myToken = Token.NewToken("test", Token.TokenTypes.STRING_LITERAL);
+        Token myTokenEof = Token.NewToken("", Token.TokenTypes.END_OF_FILE);
+        Assert.That(Token.Debug(myToken), Is.EqualTo("string: test"));
+        Assert.That(Token.Debug(myTokenEof), Is.EqualTo("END OF FILE"));
+    }
+}
