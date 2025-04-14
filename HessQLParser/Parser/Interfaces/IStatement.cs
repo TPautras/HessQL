b6cea7a1874f07303dad7aabd299495a3dd31487
TPautras <155535273+TPautras@users.noreply.github.com>
2025-04-14 23:06:09 +0200

    chore: documenting different Statements for AST

--- HessQLParser/Parser/Interfaces/IStatement.cs
+++ HessQLParser/Parser/Interfaces/IStatement.cs
@@ -1,5 +1,14 @@
 ﻿namespace HessQLParser.Parser;
-
+/// <summary>
+/// Types of Statements :
+/// BlockStatement
+///     List of statements <list type="IStatement"></list>
+/// ExpressionStatement
+///     Expression IExpression
+/// VarDeclStatement
+///     VariableName string
+///     Assigned value IExpression
+/// </summary>
 public interface IStatement
 {
     public void Statement();
