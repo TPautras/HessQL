d04804f4eaecf4cd3f1d23af72fc191aab1550e6
Amaryllis <168295590+kyuma04y@users.noreply.github.com>
2025-04-15 15:54:36 +0200

    Create Table+ DB fonctionnel csv

--- HessQLCommands/DDL-Commands.cs
+++ HessQLCommands/DDL-Commands.cs
@@ -9,11 +9,56 @@ public class DDL_Commands
         if (!Directory.Exists(CheminDB))
         {
             Directory.CreateDirectory(CheminDB);
+            string FichierDB = Path.Combine(CheminDB, NomDB + "_Description.csv");
+            File.Create(FichierDB);
         }
         else
         {
-            Console.WriteLine("CA MARCHE PAAAA");
+            Console.WriteLine("La base de donnees" + NomDB + " existe deja.");
         }
 
+    }
+
+    public static void CreateTable(string NomDB, string NomTB)
+    {
+        string CheminDB = Path.Combine(Directory.GetCurrentDirectory(), NomDB);
+        
+        if (!Directory.Exists(CheminDB))
+        {
+            Console.WriteLine("La base de donnees " + NomDB + " n'existe pas");
+        }
+        else
+        {
+            string DossierDB = Path.Combine(CheminDB, NomTB);
+            string FichierTB = Path.Combine(DossierDB, NomTB+"_Peuplement.csv");
+            string Fichier_DescriptionTB = Path.Combine(DossierDB, NomTB+"_Description.csv");
+            string Fichier_DescriptionDB = Path.Combine(CheminDB, NomDB+"_Description.csv");
+
+            if (!Directory.Exists(DossierDB))
+            {
+                Directory.CreateDirectory(DossierDB);
+            }
+
+            if (!File.Exists(FichierTB))
+            {
+                File.Create(FichierTB);
+                File.Create(Fichier_DescriptionTB);
+                try
+                {
+                    using (StreamWriter sw = File.AppendText(Fichier_DescriptionDB))
+                    {
+                        sw.WriteLine(NomTB);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("la table"+ NomTB + " existe deja.");
+            }
+        }
     }
 }
--- Sandbox/ddl.cs
+++ Sandbox/ddl.cs
@@ -2,10 +2,19 @@
 
 public class ddl
 {
-    public static void GAMBERGE()
+    public static void testDB()
     {
         Console.WriteLine("c quoi tu veux le nom");
         string nom = Console.ReadLine();
         HessQLCommands.DDL_Commands.CreateDatabase(nom);
+    }
+
+    public static void testTB()
+    {
+        Console.WriteLine("c quoi tu veux le database");
+        string nomDB = Console.ReadLine();
+        Console.WriteLine("c quoi tu veux le table");
+        string nomTB = Console.ReadLine();
+        HessQLCommands.DDL_Commands.CreateTable(nomDB, nomTB);
     }
 }
