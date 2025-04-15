ad8090ff80e28f7a003392ffaad1b931867bb51a
vlad-vrn <varenne.vw@gmail.com>
2025-04-15 16:10:05 +0200

    init HessQL-Interpreter

---
+++ HessQLInterpreter/Interpreter.cs
@@ -0,0 +1,15 @@
+﻿using HessQLParser.Parser;
+using HessQLParser.Parser.Statements;
+
+namespace HessQLInterpreter;
+
+public class Interpreter
+{
+    public void interpretCaller(BlockStmt parsedStatement)
+    {
+        foreach (IStatement statement in parsedStatement)
+        {
+            
+        }
+    }
+}
